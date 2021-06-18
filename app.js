ee6f9300f46b116b208ad646660b6c75d614500c
Pedro Henrique <phcatanduba@gmail.com>
2021-06-18 18:37:40 -0300

    customers added

--- app.js
+++ app.js
@@ -96,6 +96,137 @@ app.post('/games', async (req, res) => {
         }
     }
 });
+/*--------------------------CUSTOMERS--------------------------------*/
+
+app.get('/customers', async (req, res) => {
+    const cpf = req.query.cpf;
+    try {
+        if (!cpf) {
+            const cpfs = await connection.query('SELECT * FROM customers');
+            res.send(cpfs.rows);
+        } else {
+            const cpfs = await connection.query(
+                'SELECT * FROM customers WHERE cpf = $1',
+                ['%' + cpf + '%']
+            );
+            res.send(cpfs.rows);
+        }
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+app.get('/customers/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        const searchForID = await connection.query(
+            'SELECT * FROM customers WHERE id = $1',
+            [id]
+        );
+        if (searchForID.rows.length === 0) {
+            res.sendStatus(404);
+        } else {
+            res.send(searchForID.rows[0]);
+        }
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+app.post('/customers', async (req, res) => {
+    const validatingCustomer = req.body;
+    delete validatingCustomer.id;
+    try {
+        const hasThisCPF = await connection.query(
+            'SELECT * FROM customers WHERE cpf = $1',
+            [validatingCustomer.cpf]
+        );
+        if (hasThisCPF.rows.length === 0) {
+            const customerSchema = joi.object({
+                name: joi.string().min(1).required(),
+                phone: joi
+                    .string()
+                    .pattern(/^[0-9]+$/, 'numbers')
+                    .min(10)
+                    .max(11)
+                    .required(),
+                cpf: joi
+                    .string()
+                    .pattern(/^[0-9]+$/, 'numbers')
+                    .min(11)
+                    .max(11)
+                    .required(),
+                birthday: joi.date().required(),
+            });
+            const customer = customerSchema.validate(validatingCustomer);
+            if ('error' in customer) {
+                console.log(customer.error);
+                res.sendStatus(400);
+            } else {
+                const { name, phone, cpf, birthday } = validatingCustomer;
+                connection.query(
+                    'INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)',
+                    [name, phone, cpf, birthday]
+                );
+                res.sendStatus(201);
+            }
+        } else {
+            res.sendStatus(409);
+        }
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+app.put('/customers/:id', async (req, res) => {
+    const validatingCustomer = req.body;
+    delete validatingCustomer.id;
+    const id = parseInt(req.params.id);
+    try {
+        const searchForID = await connection.query(
+            'SELECT * FROM customers WHERE id = $1',
+            [id]
+        );
+        if (searchForID.rows.length === 0) {
+            res.sendStatus(404);
+        } else {
+            const customerSchema = joi.object({
+                name: joi.string().min(1).required(),
+                phone: joi
+                    .string()
+                    .pattern(/^[0-9]+$/, 'numbers')
+                    .min(10)
+                    .max(11)
+                    .required(),
+                cpf: joi
+                    .string()
+                    .pattern(/^[0-9]+$/, 'numbers')
+                    .min(11)
+                    .max(11)
+                    .required(),
+                birthday: joi.date().required(),
+            });
+            const customer = customerSchema.validate(validatingCustomer);
+            if ('error' in customer) {
+                console.log(customer.error);
+                res.sendStatus(400);
+            } else {
+                const { name, phone, cpf, birthday } = validatingCustomer;
+                connection.query(
+                    'UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5',
+                    [name, phone, cpf, birthday, id]
+                );
+                res.sendStatus(201);
+            }
+        }
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
 
 app.listen(4000, () => {
     console.log('rodando');
